940c1c63c3ad04a3a9af5a34b6de20efd708ca06
Gomes Moreira Sousa <42221702+Escanor2@users.noreply.github.com>
2023-08-10 15:54:22 -0300

    Update Py1.py

--- Py1.py
+++ Py1.py
@@ -1,10 +1,11 @@
 import math
-import os
 n = float(input('Digite um número '))
 n1 = int(input('Digite um número '))
 
 
 print("A raiz quadrada de {} é {:.2f}".format(n,math.sqrt(n)))
 print("A redondado pra cima de é {}".format(n,math.ceil(n)))
-#print("Os valores {} e {} são proximos ?".format(
-print(math.isclose(n, n1, rel_tol=0.05, abs_tol=0.0))
+print("Os valores {} e {} são proximos? {}"
+.format(n,n1,(math.isclose(n, n1, rel_tol=0.05, abs_tol=0.0))))
+
+
